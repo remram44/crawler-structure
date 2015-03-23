eaadfb16fe1a3818a7657d28ea6a9be349bcd685
Remi Rampin <remirampin@gmail.com>
2015-03-22 20:55:29 -0400

    Websockets!

--- crawler/main.py
+++ crawler/main.py
@@ -1,50 +1,54 @@
+from autobahn.twisted.resource import WebSocketResource
+from autobahn.twisted.websocket import WebSocketServerProtocol, \
+    WebSocketServerFactory
 import sys
 from twisted.internet import reactor
 from twisted.python import log
 from twisted.web.resource import Resource
-from twisted.web.server import Site, NOT_DONE_YET
+from twisted.web.server import Site
+from twisted.web.static import File
 
 
-class IndexPage(Resource):
-    isLeaf = True
+class UpdateWSProtocol(WebSocketServerProtocol):
+    _client_ids = 0
 
-    def _handle_err(self, e, kill_list):
-        print "canceled: %r" % (e,)
-        for c in kill_list:
-            c.cancel()
+    def __init__(self):
+        self._client_id = UpdateWSProtocol._client_ids
+        UpdateWSProtocol._client_ids += 1
+        self._socket_open = False
 
-    def render_GET(self, request):
-        print "render_GET"
-        fin = request.notifyFinish()
-        request.write('<!DOCTYPE html>\n'
-                      '<html>\n'
-                      '  <body>\n'
-                      '    <h1>Loading:</h1>\n')
-        kill_list = []
-        call = reactor.callLater(1, self._render, request, kill_list)
-        kill_list.append(call)
-        fin.addErrback(self._handle_err, kill_list)
-        return NOT_DONE_YET
+    def onOpen(self):
+        print "client %d connected" % self._client_id
+        self._socket_open = True
+        reactor.callLater(1, self._send_data)
 
-    def _render(self, request, kill_list, num=0):
-        print "_render"
-        request.write('      <p>%d</p>\n' % num)
-        if num < 10:
-            call = reactor.callLater(1, self._render, request, kill_list, num + 1)
-            kill_list[0] = call
-        else:
-            request.write('  </body>\n'
-                          '</html>\n')
-            request.finish()
-            print "finish()"
+    def onMessage(self, payload, isBinary):
+        print "got message from client %d" % self._client_id
+
+    def _send_data(self, num=0):
+        if self._socket_open:
+            print "sending message to client %d" % self._client_id
+            self.sendMessage("msg %d" % num, False)
+            reactor.callLater(1, self._send_data, num + 1)
+
+    def onClose(self, wasClean, code, reason):
+        print "client %d disconnected (%s): %s" % (
+            self._client_id,
+            "clean" if wasClean else "not clean",
+            reason)
+        self._socket_open = False
 
 
-index = IndexPage()
+root = Resource()
+root.putChild("", File('index.html'))
+updatews_factory = WebSocketServerFactory('ws://0.0.0.0:8080')
+updatews_factory.protocol = UpdateWSProtocol
+root.putChild("conn", WebSocketResource(updatews_factory))
 
 
 def main():
     log.startLogging(sys.stderr)
 
-    factory = Site(index)
+    factory = Site(root)
     reactor.listenTCP(8080, factory)
     reactor.run()
